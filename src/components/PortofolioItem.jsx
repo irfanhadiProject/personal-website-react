7bd199024b57d386146b1df32e611dd7bad978be
Irfan <irfanhadi619@gmail.com>
2025-07-16 10:30:54 +0700

    Adjust portofolio section UI

--- src/components/PortofolioItem.jsx
+++ src/components/PortofolioItem.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
 import styles from './PortofolioItem.module.css';
 
-function PortofolioItem({ link, image, alt, title, description }) {
+function PortofolioItem({ link, image, alt, title, year, description }) {
   return (
     <li className={styles.item}>
       <Link to={link}>
         <img src={image} alt={alt} className={styles.image} />
         <div className={styles.content}>
           <h3 className={styles.title}>{title}</h3>
+          <span>{year}</span>
           <p className={styles.description}>{description}</p>
         </div>
       </Link>
--- src/pages/Portofolio.jsx
+++ src/pages/Portofolio.jsx
@@ -7,19 +7,12 @@ function Portofolio() {
     <article className={styles.portofolio}>
       <Container title="My project">
         <PortofolioItem
-          link="/portofolio/birthday-invitation"
-          image="/src/assets/images/birthday-invitation.png"
-          alt="birthday invitation"
-          title="birthday party invitation"
-          description="Welcome to my Birthday Invitation! You're invited to join the celebration"
-        />
-
-        <PortofolioItem
           link="/portofolio/movie-ranking"
           image="/src/assets/images/movie-ranking.png"
           alt="movie ranking"
           title="favorite film ranking"
-          description="My personal list of favorite films, ranked from best to just awesome"
+          year="2023"
+          description="This project is a personal list of my favorite films, ranked from the most memorable to those that are still absolutely awesome. The primary goal was to practice building a clean, well-structured HTML page using only semantic HTML elements."
         />
 
         <PortofolioItem
@@ -27,7 +20,17 @@ function Portofolio() {
           image="/src/assets/images/html-only-resume.png"
           alt="html only resume"
           title="html only resume"
-          description="Welcome to my personal resume website, built entirely with plain HTML."
+          year="2023"
+          description="This project is a personal online resume, built entirely using plain HTML with no additional frameworks, styling, or scripting. The purpose of this website is to present essential information about myself including education, experience, skills, and contact details in a simple format"
+        />
+
+        <PortofolioItem
+          link="/portofolio/birthday-invitation"
+          image="/src/assets/images/birthday-invitation.png"
+          alt="birthday invitation"
+          title="birthday party invitation"
+          year="2023"
+          description="This project is a digital birthday invitation built entirely using plain HTML, with no additional styling frameworks or scripting. It serves to warmly invite guests by presenting key event details such as date, time, and location in a simple and accessible layout."
         />
       </Container>
     </article>
